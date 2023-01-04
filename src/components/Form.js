735399dce12590867cc646aa2b629c70891b606c
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2023-01-04 16:48:17 +0600

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div className=''>
+            <div>
+                <h1>Please enter your name and pick the Sectors you are currently involved in.</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
